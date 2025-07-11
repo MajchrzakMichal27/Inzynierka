d6fb048d3a7138d4d9cae63edfdb3b566ba29ac7
mm53785 <mm53785@zut.edu.pl>
2025-07-11 22:14:05 +0200

    zostalo sier modlic

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+Dziennik2024 = pd.read_excel("Dziennik2024.xlsx", sheet_name="Arkusz1")
+
+print(Dziennik2024.columns)
+
+
+
+
